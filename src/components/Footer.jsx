c466a3a94ad702b17538dddd253e7809a050a64d
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2024-03-11 08:31:51 -0400

    css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ export default function Footer(){
     return(
         <footer>
             <p>Leticia Andrade Santos</p>
+            <img class="" src="img/download.png"></img>
         </footer>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 export default function Header(){
   
     return(
-        <header>
-            <div>
-            <img id="logo" src="D:\Bibliotecas\Downloads\github-logo.png"/>
-            </div>
-            <ul>
-                <li>portugues</li>
+        <header >
+            <div class="navbar-header"> 
+            <img id="logo" src="img/github-logo.png"/>
+            <ul class="navbar-menu">
+                <li>Product</li>
+                <li>Solutions</li>
+                <li>Open Source</li>
+                <li>Pricing</li>
             </ul>
+            </div>
       </header>
     );
 }
