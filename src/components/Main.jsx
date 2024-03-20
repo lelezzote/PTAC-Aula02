3410ea1ca92a72a8942d32bd02ebf16fb604e8b0
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2024-03-20 17:13:32 -0300

    CRIACAO.USESTATE

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,40 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Main(){
-    const [nome, setNome] = useState("");7
-    return(<main>
-        <input type="text" name="" id="" onChange={ (event) => setNome(event.target.value)}/>
+    const [nome, setNome] = useState("");
+    const [telefone, setTelefone] = useState("");
+    return(
+    <main>
+        <form>
+            <label>
+        <input type="text" name="nome-contato" id="nome" value={nome} onChange={ (event) => setNome(event.target.value)}/>
+        <input type="text" name="set-telefone" id="nome" value={nome} onChange={ (event) => setTelefone(event.target.value)}/>
+        </label>
+        {nome}
+        {telefone}
+         
+
+         <button id="button">SALVAR</button>
+        
+        </form>
     </main>
 
     )
 };
+
+
+//set altera o stado
+//nome = acesso
+//setNome = altera
+
+
+//const [telefone, setTelefone] = useState("");
+//return(
+ // <main>
+ //<form>
+//<input type="text" name="nome-conato" id="nome" value={nome} onChange={ (event) => setNome(event.target.value)}/>
+ //</form>
+  //</main>
+
+
+//)
