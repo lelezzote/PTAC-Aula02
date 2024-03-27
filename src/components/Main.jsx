ec86be6d5fff0f55a5a81b053b523e26b5c1f53a
lelezzote <leticia.santos11@estudante.ifms.edu.br>
2024-03-27 17:00:32 -0300

    listaContatos

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
+ //onChange = quando algo é mudado na input, ele dispara uma funçao
+ //set salva a informaçao
+ //htmlFor serve para identificar/criar vínculo com a input
 
 export default function Main(){
     const [nome, setNome] = useState("");
-    const [telefone, setTelefone] = useState("");
+    const [telefone, setTelefone] = useState( );
+    const [listaContatos, setContatos] = useState( [ ] );
+
+    const registrar = (event) => {
+        event.preventDefault(); //previni 
+        alert("CORRETO"); 
+        setContatos([...listaContatos, 
+        {
+            nomeSalvo: nome, 
+            telefoneSalvo: telefone,
+        }
+        ]);
+    }
+    console.table(listaContatos);
     return(
     <main>
-        <form class="formulario">
+        <form class="formulario" onSubmit={registrar}>
             <div id="form-nome">
             <label class="label">Nome:</label>
         <input type="text" name="nome-contato" id="nome" value={nome} onChange={ (event) => setNome(event.target.value)}/>
@@ -17,6 +33,7 @@ export default function Main(){
          
          
          <button id="button" class="button-form">SALVAR</button>
+
         
 
         {nome}
